62bb91bca1bde1cc5e6b7a6f377388b78604a9ba
tony-decarlo-t1cg <35041071+tony-decarlo-t1cg@users.noreply.github.com>
2019-02-27 11:33:16 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.json({ msg: 'Test Works!' }));
-app.get('/hello', (req, res) => res.json({ msg: 'Hello World' }));
-app.get('/publicHello', (req, res) => res.json({ msg: 'Public Hello World' }));
+app.get('/hello', (req, res) => res.json({ msg: 'Hello World!' }));
+app.get('/publicHello', (req, res) => res.json({ msg: 'Public Hello World!' }));
 app.get('/privateHello', (req, res) =>
   res.json({ msg: 'Private Hello World' })
 );
